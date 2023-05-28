2469c3a2f4ed08469e3237b775aabb4a6a3b7a98
unknown <nidhoqed@gmail.com>
2023-05-27 20:46:36 -0700

    fix 2sum

--- 1-two-sum.py
+++ 1-two-sum.py
@@ -1,13 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
         check = {}
 
-        for i, n in enumerate(nums):
-            if (target - n) in check:
-                return [i, check[target - n]]
-            else:
-                check[n] = i
+        for i in range(len(nums)):
+            if abs(target - nums[i]) in check:
+                return [i, check[target - nums[i]]]
+            check[nums[i]] = i
 
-                
-        return [0,0]
+        return [-1,-1]
